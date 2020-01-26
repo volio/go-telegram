05aa0c98a8ad121c41551e4807216239f26415f3
volio <volio.liu@gmail.com>
2020-01-26 22:00:43 +0800

    message struct

--- model/update.go
+++ model/update.go
@@ -12,17 +12,53 @@ type Update struct {
 }
 
 type Message struct {
-	MessageID      int64    `json:"message_id"`
-	From           User     `json:"from"`
-	Chat           Chat     `json:"chat"`
-	Date           int64    `json:"date"`
-	Text           *string  `json:"text,omitempty"`
-	Sticker        *Sticker `json:"sticker,omitempty"`
-	NewChatMembers []User   `json:"new_chat_members,omitempty"`
-	LeftChatMember *User    `json:"left_chat_member,omitempty"`
-	ReplyToMessage *Message `json:"reply_to_message,omitempty"`
-	Entities       []Entity `json:"entities,omitempty"`
-	PinnedMessage  *Message `json:"pinned_message,omitempty"`
+	MessageID             int64                 `json:"message_id"`
+	From                  User                  `json:"from"`
+	Date                  int64                 `json:"date"`
+	Chat                  Chat                  `json:"chat"`
+	ForwardFrom           *User                 `json:"forward_from,omitempty"`
+	ForwardFromChat       *Chat                 `json:"forward_from_chat,omitempty"`
+	ForwardFromMessageID  *int64                `json:"forward_from_message_id,omitempty"`
+	ForwardSignature      *string               `json:"forward_signature,omitempty"`
+	ForwardSenderName     *string               `json:"forward_sender_name,omitempty"`
+	ForwardDate           *int64                `json:"forward_date,omitempty"`
+	ReplyToMessage        *Message              `json:"reply_to_message,omitempty"`
+	EditDate              *int64                `json:"edit_date,omitempty"`
+	MediaGroupID          *string               `json:"media_group_id,omitempty"`
+	AuthorSignature       *string               `json:"author_signature"`
+	Text                  *string               `json:"text,omitempty"`
+	Entities              []MessageEntity       `json:"entities,omitempty"`
+	CaptionEntities       []MessageEntity       `json:"caption_entities,omitempty"`
+	Audio                 *Audio                `json:"audio,omitempty"`
+	Document              *Document             `json:"document,omitempty"`
+	Animation             *Animation            `json:"animation,omitempty"`
+	Game                  *Game                 `json:"game,omitempty"`
+	Photo                 []PhotoSize           `json:"photo,omitempty"`
+	Sticker               *Sticker              `json:"sticker,omitempty"`
+	Video                 *Video                `json:"video,omitempty"`
+	Voice                 *Voice                `json:"voice,omitempty"`
+	VideoNote             *VideoNote            `json:"video_note,omitempty"`
+	Caption               *string               `json:"caption,omitempty"`
+	Contact               *Contact              `json:"contact,omitempty"`
+	Location              *Location             `json:"location,omitempty"`
+	Venue                 *Venue                `json:"venue,omitempty"`
+	Poll                  *Poll                 `json:"poll,omitempty"`
+	NewChatMembers        []User                `json:"new_chat_members,omitempty"`
+	LeftChatMember        *User                 `json:"left_chat_member,omitempty"`
+	NewChatTitle          *string               `json:"new_chat_title,omitempty"`
+	NewChatPhoto          []PhotoSize           `json:"new_chat_photo,omitempty"`
+	DeleteChatPhoto       *bool                 `json:"delete_chat_photo,omitempty"`
+	GroupChatCreated      *bool                 `json:"group_chat_created,omitempty"`
+	SuperGroupChatCreated *bool                 `json:"supergroup_chat_created,omitempty"`
+	ChannelChatCreated    *bool                 `json:"channel_chat_created,omitempty"`
+	MigrateToChatID       *int64                `json:"migrate_to_chat_id,omitempty"`
+	MigrateFromChatID     *int64                `json:"migrate_from_chat_id,omitempty"`
+	PinnedMessage         *Message              `json:"pinned_message,omitempty"`
+	Invoice               *Invoice              `json:"invoice,omitempty"`
+	SuccessfulPayment     *SuccessfulPayment    `json:"successful_payment,omitempty"`
+	ConnectedWebsite      *string               `json:"connected_website,omitempty"`
+	PassportData          *PassportData         `json:"passport_data,omitempty"`
+	ReplyMarkup           *InlineKeyboardMarkup `json:"reply_markup,omitempty"`
 }
 
 type User struct {
@@ -55,7 +91,7 @@ type Sticker struct {
 	FileSize     *int64     `json:"file_size,omitempty"`
 }
 
-type Entity struct {
+type MessageEntity struct {
 	Type     string  `json:"type"`
 	Offset   int64   `json:"offset"`
 	Length   int64   `json:"length"`
@@ -67,7 +103,106 @@ type Entity struct {
 type PhotoSize struct {
 	FileID       string `json:"file_id"`
 	FileUniqueID string `json:"file_unique_id"`
-	FileSize     int64  `json:"file_size"`
 	Width        int64  `json:"width"`
 	Height       int64  `json:"height"`
+	FileSize     *int64 `json:"file_size,omitempty"`
+}
+
+type Audio struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Duration     int64      `json:"duration"`
+	Performer    *string    `json:"performer,omitempty"`
+	Title        *string    `json:"title,omitempty"`
+	MimeType     *string    `json:"mime_type,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+}
+
+type Document struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+	FileName     *string    `json:"file_name,omitempty"`
+	MimeType     *string    `json:"mime_type,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+}
+
+type Animation struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Width        int64      `json:"width"`
+	Height       int64      `json:"height"`
+	Duration     int64      `json:"duration"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+	FileName     *string    `json:"file_name,omitempty"`
+	MimeType     *string    `json:"mime_type,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+}
+
+type Game struct {
+	Title        string          `json:"title"`
+	Description  string          `json:"description"`
+	Photo        []PhotoSize     `json:"photo"`
+	Text         *string         `json:"text,omitempty"`
+	TextEntities []MessageEntity `json:"text_entities,omitempty"`
+	Animation    *Animation      `json:"animation,omitempty"`
+}
+
+type Video struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Width        int        `json:"width"`
+	Height       int        `json:"height"`
+	Duration     int        `json:"duration"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+	MimeType     *string    `json:"mime_type,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+}
+
+type Voice struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Length       int64      `json:"length"`
+	Duration     int64      `json:"duration"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+}
+
+type VideoNote struct {
+	FileID       string     `json:"file_id"`
+	FileUniqueID string     `json:"file_unique_id"`
+	Length       int64      `json:"length"`
+	Duration     int64      `json:"duration"`
+	Thumb        *PhotoSize `json:"thumb,omitempty"`
+	FileSize     *int64     `json:"file_size,omitempty"`
+}
+
+type Contact struct {
+	PhoneNumber string  `json:"phone_number"`
+	FirstName   string  `json:"first_name"`
+	LastName    *string `json:"last_name,omitempty"`
+	UserID      *int64  `json:"user_id,omitempty"`
+	VCard       *string `json:"vcard,omitempty"`
+}
+
+type Location struct {
+}
+
+type Venue struct {
+}
+
+type Poll struct {
+}
+
+type Invoice struct {
+}
+
+type SuccessfulPayment struct {
+}
+
+type PassportData struct {
+}
+
+type InlineKeyboardMarkup struct {
 }
